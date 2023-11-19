ee96ec051f3cac3f9aa4bfeedb9ea659f1143027
Dirk Beer <rdbeer@pm.me>
2023-11-19 11:12:20 -0800

    Update run_analysis.sh

--- run_analysis.sh
+++ run_analysis.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+h#!/bin/bash
 
 # Change to the directory of the script
 cd "$(dirname "$0")"
@@ -15,7 +15,8 @@ if [[ "$VIRTUAL_ENV" != "" ]]; then
 
     # Check if analyze.py ran successfully
     if [[ $? -eq 0 ]]; then
-        echo "Analysis complete. The output has been saved."
+        echo ""
+        echo "Analysis complete."
 
         # Attempt to retrieve the local IP address of the Raspberry Pi
         IP_ADDRESS=$(hostname -I | awk '{print $1}')
@@ -26,10 +27,13 @@ if [[ "$VIRTUAL_ENV" != "" ]]; then
 
         # Copy the image to the web server directory
         sudo cp ./receiver_performance.png /usr/local/share/tar1090/html
-        echo "The image has been copied to the web server directory."
+        echo "The image has been copied to the web server tar1090 directory."
 
         # Provide the user with the URL to view the image
-        echo "Go view the receiver performance plot at http://$IP_ADDRESS/tar1090/receiver_performance.png"
+        echo "You can view the receiver performance plot at"
+        echo ""
+        echo "    http://$IP_ADDRESS/tar1090/receiver_performance.png"
+        echo ""
 
     else
         echo "An error occurred while running the analysis."
