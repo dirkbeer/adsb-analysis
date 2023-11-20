96870c40299a5f1bc188c9361a3e6ce73c4c9c26
Dirk Beer <dirk@dirkbeer.tech>
2023-11-19 16:47:45 -0800

    --use-all by default in the run script

--- run_analysis.sh
+++ run_analysis.sh
@@ -10,9 +10,9 @@ source venv/bin/activate
 if [[ "$VIRTUAL_ENV" != "" ]]; then
     echo "Virtual environment activated."
 
-    # Run the analysis script
-    ./analyze.py
-    #./analyze.py --use-all
+    # Run the analysis script (only uncomment one of these)
+    #./analyze.py
+    ./analyze.py --use-all
 
     # Check if analyze.py ran successfully
     if [[ $? -eq 0 ]]; then
